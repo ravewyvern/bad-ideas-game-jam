fe11141eaad40e363665dc490b09d33c4133079f
ChaoticCrits <justinhagerman97@gmail.com>
2026-03-15 15:40:36 -0400

    Player moved to Level 1

--- Scripts/menus/main_menu.gd
+++ Scripts/menus/main_menu.gd
@@ -8,7 +8,7 @@ extends VBoxContainer
 
 func _on_level_button_down() -> void:
 	# load into main game, should eventually open a menu to select which level to play
-	get_tree().change_scene_to_file("res://Scenes/Maps/Game.tscn")
+	get_tree().change_scene_to_file("res://Scenes/Maps/Level_1.tscn")
 
 func _on_options_button_down() -> void:
 	 # many add options menu eventually? if not just delete this and the button
